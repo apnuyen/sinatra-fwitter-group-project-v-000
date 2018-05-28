ad78dfe2ed0b6ca6286d6f976071fc1183856ba7
An Nuyen <annuyen@gmail.com>
2018-05-28 19:31:31 +0000

    automatically backed up by learn

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -36,7 +36,7 @@ class UsersController < ApplicationController
     @user = User.find_by(:username => params[:username])
     if @user && @user.authenticate(params[:password])
       session[:user_id] = @user.id
-      redirect to '/tweets'
+      redirect to :'/tweets'
     else
       flash[:message] = "login info incorrect"
       redirect to '/login'
