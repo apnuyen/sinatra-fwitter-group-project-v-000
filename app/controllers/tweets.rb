a0d0e432fab495cbabec9939acd07cd12a974367
An Nuyen <annuyen@gmail.com>
2018-05-28 19:02:15 +0000

    automatically backed up by learn

--- app/controllers/tweets.rb
+++ app/controllers/tweets.rb
@@ -15,15 +15,92 @@ class TweetsController < ApplicationController
     else
       redirect to '/login'
     end
-  end 
+  end
 
-  post '/tweets' do 
+  post '/tweets' do
     if params[:content] != ""
       @tweet = Tweet.create(:content => params[:content], :user_id => session[:user_id])
-      @tweet.save 
-      redirect to "/tweets" 
+      @tweet.save
+      redirect to "/tweets"
+    else
+      flash[:message] = "You must enter tweet content"
+      redirect to "/tweets/new"
+    end
+  end
+
+  get '/tweets/:id' do 
+    if logged_in? 
+      @tweet = Tweet.find(params[:id]) 
+      erb :'/tweets/show_tweet' 
     else 
-      flash[:message] = "You must enter tweet content" 
-      redirect to "/tweets/new" 
+      redirect to '/login' 
     end 
   end 
+
+  get '/tweets/:id/edit' do 
+    if logged_in? 
+      @tweet = Tweet.find(params[:id])
+      erb :'/tweets/edit_tweet' 
+    else 
+      redirect to '/login' 
+    end 
+  end 
+
+  path '/tweets/:id' do 
+    @tweet = Tweet.find(params[:id]) 
+    if params[:content] != "" 
+      @tweet.update(:content => params[:content])
+      flash[:message] = "Tweet has been updated" 
+      redirect to "/tweets/#{@tweet.id}" 
+    else 
+      flash[:message] = "Please enter a tweet" 
+      redirect to "/tweets/#{@tweet.id}/edit" 
+    end 
+  end 
+
+  delete '/tweets/:id/delete' do 
+    @tweet = Tweet.find(params[:id]) 
+    if @tweet.user_id == session[:user_id] 
+      @tweet = Tweet.delete(params[:id])
+      redirect to '/tweets' 
+    else 
+      redirect to '/tweets' 
+    end 
+  end 
+
+end 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      
