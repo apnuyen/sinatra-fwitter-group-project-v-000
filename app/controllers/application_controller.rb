b5ede4eb82ffde9cf74f2773cd444746638c1b21
An Nuyen <annuyen@gmail.com>
2018-05-28 18:53:42 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,23 +1,28 @@
 require './config/environment'
+require 'rack-flash'
 
 class ApplicationController < Sinatra::Base
+  user Rack::Flash
 
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "secret"
   end
-  
+
   get '/' do
     erb :index
   end
 
-  get '/tweets/new' do
-    erb :'tweets/new'
-  end
+  helpers do
+    def logged_in?
+      !!session[:user_id]
+    end
 
-  post '/tweets' do
-    @tweet = Tweet.create(params[:tweet])
-    erb :'/tweets/show_tweet'
+    def current_user
+      User.find(session[:user_id])
+    end
   end
 
 end
