f48e38de84a97f2ba690eb51ce0defb92210ba8b
An Nuyen <annuyen@gmail.com>
2018-05-28 18:57:15 +0000

    automatically backed up by learn

--- app/controllers/tweets.rb
+++ app/controllers/tweets.rb
@@ -1,18 +1,29 @@
-class TweetsController < ApplicationController 
+class TweetsController < ApplicationController
 
-  get '/tweets' do 
-    if !logged_in? 
-      redirect to '/login' 
-    else 
+  get '/tweets' do
+    if !logged_in?
+      redirect to '/login'
+    else
       @user = User.find(session[:user_id])
       erb :'/tweets/tweets'
-    end 
+    end
+  end
+
+  get '/tweets/new' do
+    if logged_in?
+      erb :'tweets/create_tweet'
+    else
+      redirect to '/login'
+    end
   end 
 
-  get '/tweets/new' do 
-    if logged_in? 
-      erb :'tweets/create_tweet' 
+  post '/tweets' do 
+    if params[:content] != ""
+      @tweet = Tweet.create(:content => params[:content], :user_id => session[:user_id])
+      @tweet.save 
+      redirect to "/tweets" 
     else 
-      redirect to '/login' 
+      flash[:message] = "You must enter tweet content" 
+      redirect to "/tweets/new" 
     end 
   end 
